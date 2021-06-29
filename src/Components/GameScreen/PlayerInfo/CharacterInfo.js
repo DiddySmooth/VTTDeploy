4e48f516613326d22729e6ca2a002582277de435
DiddySmooth <76023664+DiddySmooth@users.noreply.github.com>
2021-06-29 15:28:07 -0500

    Created Character stats and info

--- src/Components/GameScreen/PlayerInfo/CharacterInfo.js
+++ src/Components/GameScreen/PlayerInfo/CharacterInfo.js
@@ -1,8 +1,22 @@
 const CharacterInfo = () => {
     return (
-        <div>
-            <label>Name:</label>
-            <span> Grayson </span>
+        <div className= "characterInfoContainer">
+            <div className= "characterInfoContainer">
+                <label>Name:</label>
+                <span> Grayson </span>
+            </div>
+            <div className= "characterInfoContainer">
+                <label>Race:</label>
+                <span> Ogre </span>
+            </div>
+            <div className= "characterInfoContainer">
+                <label>Class:</label>
+                <span> Rogue </span>
+            </div>
+            <div className= "characterInfoContainer">
+                <label>Level:</label>
+                <span> 5 </span>
+            </div>
         </div>
     )
 }
